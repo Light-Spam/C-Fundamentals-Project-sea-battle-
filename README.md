dd4537277b383f99fc522de76676cb48bfa44bff
Light-Spam <ali.fam0079@gmail.com>
2021-02-07 18:29:59 +0330

    adding header file and making ready to merge

--- README.md
+++ README.md
@@ -5,25 +5,13 @@
 
 **REFACTORS :**
 
-refactor : changing SetColor  to  setColor
-
-refactor : in func "drawMap" -> changing StatusArray[][]  to   sea[][]
-
-refactor : in func "drawMap" -> got rid of alphabets, now we deal with numbers only
+- Now our base and most used methods are in a new header file.
 
 **FEATS :**
 
-feat : adding ships structure
-
-feat : adding a function to define area of ban for every ship (corners checked)
-
-feat : new color added -> purple
 
 **FIX :** 
-
-fix : X and Y were vice versa in draw function
 
 
 
 **NOTE :**
-some test ships are build in main.
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-
+#include "BaseMethods.h"
 #define MAX_SHIP_SIZE 10
 
 typedef struct shipNode {
@@ -74,7 +74,7 @@ void banLocation(struct shipNode ship, char position, int row, int cols, char se
             if (((ty - 1) + a <= 0) || (tx - 1) <= 0) continue;
             sea[(tx - 1)][(ty - 1) + a] = 'b';
         }
-        
+
         for (int a = 0; a <= ship.size + 1; a++) {
             if (ty + 1 <= 0) continue;
             sea[(tx + 1)][((ty - 1) + a)] = 'b';
@@ -85,105 +85,8 @@ void banLocation(struct shipNode ship, char position, int row, int cols, char se
 
 }
 
-// just changes font color.
-void setColor(char chosenColor) {
-    if (chosenColor == 'b' || chosenColor == 'B') {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
-                                FOREGROUND_GREEN |
-                                FOREGROUND_BLUE);
-    }
-    if (chosenColor == 'r' || chosenColor == 'R') {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
-                                FOREGROUND_RED);
-    }
-    if (chosenColor == 'y' || chosenColor == 'Y') {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
-                                FOREGROUND_RED | FOREGROUND_GREEN);
-    }
-    if (chosenColor == 'p' || chosenColor == 'P') {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
-                                FOREGROUND_RED | FOREGROUND_BLUE);
-
-    }
-
-}
-
-// makes the sea and set every block to "."
-void makeDefaultStatusArray(int rows, int cols, char sea[rows][cols]) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            sea[i][j] = '.';
-        }
-    }
-}
-
-//                       WARNING: this method works with both banLocation and ship structure.
-//                                if data's are not sync, this will fail.
-void drawMap(int rows, int cols, char sea[rows][cols]) {
-    char sp = ' ';
-
-// Line 1 Numbers
-    printf("%c%c%c%c", sp, sp, sp, sp);
-    for (int a = 0; a < cols; a++) {
-        setColor('y');
-        printf("%d%c%c%c%c%c%c%c", 0 + a, sp, sp, sp, sp, sp, sp, sp);
-        fflush(stdout);
-    }
-    printf("\n");
-    setColor('p');
-//first row (3 lines) with no number
-    for (int b = 0; b < rows; b++) {
 
 
-        // Seprator Line after 3 lines in one row
-        for (int a = 0; a < cols; a++) {
-            printf(" -------");
-            fflush(stdout);
-        }
-        printf("\n");
-
-        //row 1 line 1 / 3
-        printf("|");
-        for (int a = 0; a < cols; a++) {
-            printf("%c%c%c%c%c%c%c|", sp, sp, sp, sp, sp, sp, sp);
-            fflush(stdout);
-        }
-        printf("\n");
-
-        //row 1 line 2 / 3
-        setColor('y');
-        printf("%d", b);
-        fflush(stdout);
-        setColor('p');
-        fflush(stdout);
-
-        for (int a = 0; a < cols; a++) {
-            setColor('b');
-            char ship = sea[a][b];  //---------------------------------CHARACTER IS HERE
-            printf("%c%c%c%c%c%c%c", sp, sp, sp, ship, sp, sp, sp);
-            fflush(stdout);
-            setColor('p');
-            printf("|");
-            fflush(stdout);
-            setColor('p');
-        }
-        printf("\n");
-        //row 1 line 3/3
-        printf("|");
-        for (int a = 0; a < cols; a++) {
-            printf("%c%c%c%c%c%c%c|", sp, sp, sp, sp, sp, sp, sp);
-            fflush(stdout);
-        }
-        printf("\n");
-    }
-    // Last line --- ( out of loop)
-    for (int a = 0; a < cols; a++) {
-        printf(" -------");
-        fflush(stdout);
-    }
-    printf("\n");
-
-}
 
 
 int main() {
